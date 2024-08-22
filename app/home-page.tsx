b9b3c20cf655ffc855785a2ca8b0901cf016ad4e
aakash110121 <aakash.avology@gmail.com>
2024-08-22 17:27:42 +0530

    About page added

--- app/home-page.tsx
+++ app/home-page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Link from "next/link";
 import { useState } from "react";
 import Image from 'next/image';
 import Testimonial1 from '@/public/images/testimonial-01.jpg';
@@ -51,6 +50,24 @@ export default function HomePage() {
 
   return (
     <>
+      {/* Page illustration */}
+      <div className="cey85 czd2q czjaw cmhb9 codcr c7k83" aria-hidden="true">
+        <svg className="cem1n cimtg cgdju cgk3d c2s69 c83d2 cti3h" width="800" height="502" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="400" cy="102" r="400" fill="url(#heroglow_paint0_radial)" fillOpacity=".6"></circle>
+          <circle cx="209" cy="289" r="170" fill="url(#heroglow_paint1_radial)" fillOpacity=".4"></circle>
+          <defs>
+            <radialGradient id="heroglow_paint0_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="rotate(90 149 251) scale(315.089)">
+              <stop stopColor="#b70000"></stop>
+              <stop offset="1" stopColor="#b70000" stopOpacity=".01"></stop>
+            </radialGradient>
+            <radialGradient id="heroglow_paint1_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="rotate(90 -40 249) scale(133.913)">
+              <stop stopColor="#000"></stop>
+              <stop offset="1" stopColor="#000" stopOpacity=".01"></stop>
+            </radialGradient>
+          </defs>
+        </svg>
+      </div>
+
       {/* Hero */}
       <section>
         <div className="czd2q c92f3 cmhb9 c1plj">
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,23 +19,6 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
           <UserStatesProvider>
             <Header></Header>
             <main className="c2h4q">
-              {/* Page illustration */}
-              <div className="cey85 czd2q czjaw cmhb9 codcr c7k83" aria-hidden="true">
-                <svg className="cem1n cimtg cgdju cgk3d c2s69 c83d2 cti3h" width="800" height="502" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="400" cy="102" r="400" fill="url(#heroglow_paint0_radial)" fillOpacity=".6"></circle>
-                  <circle cx="209" cy="289" r="170" fill="url(#heroglow_paint1_radial)" fillOpacity=".4"></circle>
-                  <defs>
-                    <radialGradient id="heroglow_paint0_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="rotate(90 149 251) scale(315.089)">
-                      <stop stopColor="#b70000"></stop>
-                      <stop offset="1" stopColor="#b70000" stopOpacity=".01"></stop>
-                    </radialGradient>
-                    <radialGradient id="heroglow_paint1_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="rotate(90 -40 249) scale(133.913)">
-                      <stop stopColor="#000"></stop>
-                      <stop offset="1" stopColor="#000" stopOpacity=".01"></stop>
-                    </radialGradient>
-                  </defs>
-                </svg>
-              </div>
               {children}
             </main>
             <Footer></Footer>
