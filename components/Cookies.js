da98e25537281416508fc3c65cfc74ac1a976dfe
aakash110121 <aakash.avology@gmail.com>
2024-09-03 11:07:42 +0530

    lowercase issue fixed

---
+++ components/Cookies.js
@@ -0,0 +1,50 @@
+'use client'
+import { useState, useEffect } from 'react';
+
+export default function Cookies() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    // Check localStorage for user consent status
+    const consentStatus = localStorage.getItem('cookieConsent');
+    if (!consentStatus) {
+      // Show popup if there's no consent status in localStorage
+      setIsVisible(true);
+    }
+  }, []);
+
+  const handleAccept = () => {
+    localStorage.setItem('cookieConsent', 'accepted');
+    setIsVisible(false);
+  };
+
+  const handleDecline = () => {
+    localStorage.setItem('cookieConsent', 'declined');
+    setIsVisible(false);
+  };
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="cookies-wrap">
+        <div className="wrapper">
+          <header>
+            <h2>Cookies Consent</h2>
+          </header>
+
+          <div className="data">
+            <p>This website use cookies to help you have a superior and more relevant browsing experience on the website.</p>
+          </div>
+
+          <div className="buttons">
+            <button className="button" id="acceptBtn" onClick={handleAccept}>Accept</button>
+            <button className="button" id="declineBtn" onClick={handleDecline}>Decline</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
